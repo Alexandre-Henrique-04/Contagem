022595466acf1deb3497b319bbe43d391b8c7ebc
alex15grande <alex15grande@hotmail.com>
2023-08-20 16:27:55 -0300

    Projeto Finalizado

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let numero = window.document.querySelector('input#fnumber');
+let lista = window.document.querySelector('select#flista');
+let res = window.document.querySelector('div#resultado');
+let valores = [];
+
+function gerador(){
+
+}
